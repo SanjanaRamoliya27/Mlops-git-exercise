c41259a4c923c3f7ce10b6051da46af1547c469c
SanjanaRamoliya27 <sanjanaramoliya2712@gmail.com>
2025-12-15 18:54:30 +0530

    Updated output message

--- hello_world.py
+++ hello_world.py
@@ -4,6 +4,6 @@ class HelloWorld:
         print("Hello World from Git & GitHub")
         print("This project demonstrates basic Git and GitHub workflow.")
         print("Repository successfully connected and code pushed to GitHub.")
-
+        print("Author: Sanjana Ramoliya")
 if __name__ == "__main__":
     HelloWorld.main()
